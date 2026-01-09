9f401175cf9b5695e8f283f710dc05564e1772f1
magmag1351 <kenta7350@icloud.com>
2026-01-09 10:26:22 +0900

    メンバーリストのレコードを全て表示するように変更

--- LabRev/app/pages/index.vue
+++ LabRev/app/pages/index.vue
@@ -207,7 +207,7 @@ watch(() => statusFilter.value, (newVal) => {
 
 const pagination = ref({
   pageIndex: 0,
-  pageSize: 10
+  pageSize: 100
 })
 </script>
 
@@ -317,21 +317,7 @@ const pagination = ref({
         }"
       />
 
-      <div class="flex items-center justify-between gap-3 border-t border-default pt-4 mt-auto">
-        <div class="text-sm text-muted">
-          {{ table?.tableApi?.getFilteredSelectedRowModel().rows.length || 0 }} of
-          {{ table?.tableApi?.getFilteredRowModel().rows.length || 0 }} row(s) selected.
-        </div>
 
-        <div class="flex items-center gap-1.5">
-          <UPagination
-            :default-page="(table?.tableApi?.getState().pagination.pageIndex || 0) + 1"
-            :items-per-page="table?.tableApi?.getState().pagination.pageSize"
-            :total="table?.tableApi?.getFilteredRowModel().rows.length"
-            @update:page="(p: number) => table?.tableApi?.setPageIndex(p - 1)"
-          />
-        </div>
-      </div>
     </template>
   </UDashboardPanel>
 </template>
